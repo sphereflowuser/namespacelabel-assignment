77d33e3ef4d747011240ec92ffd37901126643a0
matanamar10 <matanamar76@gmail.com>
2024-03-07 19:37:09 +0000

    editing the relevent files to implement validating admission webhook

--- cmd/main.go
+++ cmd/main.go
@@ -128,6 +128,10 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "NamespaceLabel")
 		os.Exit(1)
 	}
+	if err = (&danaiov1alpha1.NamespaceLabel{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "NamespaceLabel")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
